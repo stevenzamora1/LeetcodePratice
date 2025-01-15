287f9462b045f8874dba2b42e43b7177f1b5276e
stevenzamora1 <stevenzamora205@gmail.com>
2025-01-15 02:13:12 -0500

    added isValid problem

---
+++ data-strut-pratice/stacks.py
@@ -0,0 +1,11 @@
+# A stack is last in - first out
+browsing_session = []
+browsing_session.append(1)
+browsing_session.append(2)
+browsing_session.append(3)
+print(browsing_session)
+last = browsing_session.pop()
+print(last)
+print(browsing_session[-1])
+if not browsing_session:
+    print("disable")
